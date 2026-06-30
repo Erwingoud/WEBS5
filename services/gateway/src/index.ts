f2a0e4de37cb4615b89c0af519b6f5a149b92be1
Erwingoud <140809838+Erwingold@users.noreply.github.com>
2026-06-30 10:51:18 +0200

    feat: added swagger docs

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import authRouter from "./routers/authRoute";
 import targetRouter from "./routers/targetRoute";
 import { env } from "./env";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json";
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.get("/health", (_req, res) => {
 
 app.use("/auth", authRouter);
 app.use("/targets", targetRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(env.port, () => {
   console.log(`Gateway microservice started on port ${env.port}`);
